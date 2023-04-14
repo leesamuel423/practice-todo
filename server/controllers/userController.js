dd05590cc861a9d194b4c8244c8006ce4ff9aecd
Samuel Lee <leesamuel423@gmail.com>
2023-04-14 17:42:58 -0400

    update errors with async implementation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/userModel.js');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
 const userController = {};
 
@@ -19,23 +19,25 @@ userController.signup = async (req, res, next) => {
 
   try {
     // check to see if username is in the database
-    User.findOne({ username }).exec()
-      .then(user => {
-        // if username is in database, throw error
-        if (user) {
-          res.status(400);
-          throw new Error('Username already exists. Please choose another username');
-        }
-        // if username is not in database, create new user with bcrypt hashed password
-        User.create({ username, password })
-          .then(user => {
-            res.locals.user = user;
-            return next();
-          });
-      });
-  } 
+    const user = await User.findOne({ username }).exec();
+    // if username is in database, throw error
+    if (user) {
+      res.status(400);
+      throw new Error('Username already exists. Please choose another username');
+    }
+    // if username is not in database, create new user with bcrypt hashed password
+    const newUser = User.create({ username, password });
+    res.locals.user = newUser;
+    return next();
+  }
   catch (err) {
     res.status(400);
     throw new Error('Error in userController.login controller');
   }
 };
+
+
+
+
+// Export statement
+module.exports = userController;
